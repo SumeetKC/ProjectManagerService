6a96a8c91c963fa3d64c2b2f0d247a57f1e8e9df
sumeetkc <SU98su@&>
2022-08-25 22:05:23 +0530

    Added group

--- src/main/java/com/projectmanager/model/TestData.java
+++ src/main/java/com/projectmanager/model/TestData.java
@@ -6,6 +6,16 @@ public class TestData {
     public String name;
     private String urlInfo;
 
+    private String group;
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
     public String getName() {
         return name;
     }
